60b12b2fb0b656e6eba63651f5d3e31f4f88d7bb
Jason Abbott <dev@trailimage.com>
2018-01-01 14:16:19 -0700

    remove comment

--- src/storage.test.ts
+++ src/storage.test.ts
@@ -4,5 +4,4 @@ test("Save numbers", () => {
    session.save("test-key", 99);
    expect(sessionStorage.setItem).toHaveBeenLastCalledWith("test-key", "99");
    expect(sessionStorage.__STORE__["test-key"]).toBe("99");
-   //expect(session.item("test-key")).toBe(99);
 });
