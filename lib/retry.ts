7b3e660c9f9ff2d2e71942a109e4f4266b7d8b9c
Jason Abbott <dev@trailimage.com>
2018-03-04 13:29:06 -0700

    add tests for retry wrapper

--- lib/retry.ts
+++ lib/retry.ts
@@ -1,28 +1,41 @@
 /**
- * Retry a function until it resolves or number of retry times is reached.
+ * Retry a `Promise` function until it resolves or number of retry times is
+ * exceeded.
  *
  * @param times Number of times to retry
  * @param delay Milliseconds between retries
  */
-export function retry<T>(
+export const retry = <T>(
    fn: () => Promise<T>,
    times: number,
    delay: number
-): Promise<T> {
-   return retryLoop<T>(fn, times, delay);
-}
+): Promise<T> =>
+   times < 0 || delay < 0
+      ? Promise.reject(`Invalid retry times (${times}) or delay (${delay})`)
+      : new Promise((resolve, reject) => {
+           retryLoop<T>(fn, times, delay, resolve, reject);
+        });
 
 function retryLoop<T>(
    fn: () => Promise<T>,
    times: number,
    delay: number,
-   count: number = 1
-): Promise<T> {
-   return fn().catch(err => {
-      if (count < times) {
-         setTimeout(() => {
-            retryLoop(fn, times, delay, count + 1);
-         }, delay);
-      }
-   });
+   resolve: (res: T) => void,
+   reject: (errors: string[]) => void,
+   count: number = 1,
+   errors: string[] = []
+): void {
+   fn()
+      .then(res => resolve(res))
+      .catch(err => {
+         errors.push(err.toString());
+
+         if (count < times) {
+            setTimeout(() => {
+               retryLoop(fn, times, delay, resolve, reject, count + 1, errors);
+            }, delay);
+         } else {
+            reject(errors);
+         }
+      });
 }
--- lib/storage.test.ts
+++ lib/storage.test.ts
@@ -1,5 +1,10 @@
 import { session } from '../index';
 
+/**
+ * Jest testing uses `jest-localstorage-mock`.
+ *
+ * https://github.com/clarkbw/jest-localstorage-mock
+ */
 test('Save numbers', () => {
    session.save('test-key', 99);
    expect(sessionStorage.setItem).toHaveBeenLastCalledWith('test-key', '99');
