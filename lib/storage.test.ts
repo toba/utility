1bc1899c3c6495f26d5e7c796e3fce5581fe0694
Jason Abbott <dev@trailimage.com>
2018-08-11 14:11:00 -0600

    update deps

--- lib/storage.test.ts
+++ lib/storage.test.ts
@@ -1,13 +1,7 @@
 import '@toba/test';
 import { session } from './index';
 
-/**
- * Jest testing uses `jest-localstorage-mock`.
- *
- * https://github.com/clarkbw/jest-localstorage-mock
- */
 test('save numbers', () => {
    session.save('test-key', 99);
-   expect(sessionStorage.setItem).toHaveBeenLastCalledWith('test-key', '99');
-   expect(sessionStorage.__STORE__['test-key']).toBe('99');
+   expect(session.item('test-key')).toBe(99);
 });
